0a8e3f4df8a25b8d92c0c73b218b11a54380b0e8
Laura Gao <gaolauro@gmail.com>
2023-07-13 22:21:46 -0400

    initial commit, v incomplete

---
+++ setup.sh
@@ -0,0 +1,13 @@
+sudo apt install git
+sudo apt install vim
+sudo apt install npm
+sudo apt install code --classic
+sudo apt install python-pip
+
+git config --global user.enail gaolauro@gmail.com
+git config --global user.name "Laura Gao"
+git config --global init.defaultBranch main
+
+code --install-extension esbenp.prettier-vscode
+code --install-extension vscodevim # ??
+# tailwindcss intellisense
